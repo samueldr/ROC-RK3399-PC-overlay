b902a61bbc9d761d22fdbb187eddc3b440590c4c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-02-16 12:04:34 -0500

    overlay: Pass pkgsCross directly to simplify things

--- default.nix
+++ default.nix
@@ -7,6 +7,8 @@
 # Makes the ROC-RK3399-PC attrset available directly
 pkgs.ROC-RK3399-PC //
 {
+  # Pass through the cross-compilation infra
+  inherit (pkgs) pkgsCross;
   # And passes the nixpkgs with overly through if required.
   overlay = pkgs;
 }
